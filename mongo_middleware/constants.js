8e9df3c58a44bf8fd0e13da01a98f0d093fd3f2c
MartaMoran <62018823+MartaMoran@users.noreply.github.com>
2020-08-31 20:00:19 +0200

    Update constants.js

--- mongo_middleware/constants.js
+++ mongo_middleware/constants.js
@@ -1,6 +1,6 @@
 const MONGO_URL = "clusterfirst.1cyh6.mongodb.net";
 const EXPORT_USER = "MartaMoranFernandez";
-const MONGO_PSW = "yosociologa";
+
 const MONGO_DB = "sample_mflix";
 
 export { MONGO_URL, MONGO_PSW, EXPORT_USER, MONGO_DB };
